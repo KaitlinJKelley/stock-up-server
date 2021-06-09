2447bc7b4a9c80c06cc7a2b22d0942bece389e72
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-09 17:03:03 -0500

    Added company_parts fixture; Added create function to ProductViewSet, which adds product and productpart to database

--- stockupapi/views/product.py
+++ stockupapi/views/product.py
@@ -1,4 +1,4 @@
-from stockupapi.models.company_parts import CompanyPart
+from stockupapi.models.product_company_part import ProductPart
 from stockupapi.models.parts import Part
 from django.core.exceptions import ValidationError
 from rest_framework import status
@@ -6,7 +6,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from stockupapi.models import Company, Product
+from stockupapi.models import Company, Product, CompanyPart
 
 class ProductViewSet(ViewSet):
 
@@ -18,12 +18,39 @@ class ProductViewSet(ViewSet):
         serializer = ProductSerializer(products, many=True, context={'request': request})
 
         return Response(serializer.data["id"], serializer.data["name"])
+    
+    def create(self, request):
+        # TODO: Add image handling
+
+        company = Company.objects.get(employee__user = request.auth.user)
+
+        product = Product()
+        product.company = company
+        product.name = request.data["name"]
+
+        product.save()
+
+        for part in request.data["parts"]:
+            company_part = CompanyPart.objects.get(pk=part["partId"])
+            
+            product_part = ProductPart()
+
+            product_part.product = product
+            product_part.company_part = company_part
+            product_part.amount_used = part["amountUsed"]
+
+            product_part.save()
+
+        serializer = ProductSerializer(product, many=False, context={'request': request})
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class ProductSerializer(serializers.ModelSerializer):
-        
+
         class Meta:
             model = Product
 
             fields = ('id', 'name', 'parts')
+            depth = 1
 
     
