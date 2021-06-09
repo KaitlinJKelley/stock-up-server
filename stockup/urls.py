3fcca00832047fa262ba09206b0d2aec97e36b36
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-09 11:44:09 -0500

    added and loaded token.json fixture; started urls.py

--- stockup/urls.py
+++ stockup/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from stockupapi.models import *
+from stockupapi.views import *
+
+router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include(router.urls))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
