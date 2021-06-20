c668f74e73ea043b167c26c2c9c965ff64741012
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-20 11:00:05 -0500

    Added create function in UnitOfMeasurementViewSet

--- stockupapi/views/unit_of_measurement.py
+++ stockupapi/views/unit_of_measurement.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from stockupapi.models import UnitOfMeasurement
+from rest_framework import status
 
 class UnitOfMeasurementViewSet(ViewSet):
     def list(self, request):
@@ -10,6 +11,16 @@ class UnitOfMeasurementViewSet(ViewSet):
         serializer = UOMSerializer(unit_of_measurement, many=True, context={'request': request})
 
         return Response(serializer.data)
+
+    def create(self, request):
+        uom = UnitOfMeasurement()
+        uom.label = request.data["label"]
+
+        uom.save()
+
+        serializer = UOMSerializer(uom, context={'request': request})
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class UOMSerializer(serializers.ModelSerializer):
 
