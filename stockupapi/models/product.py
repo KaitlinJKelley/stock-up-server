fa1eea4d7203fb59b5edf068b89f77735c0e9952
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-23 14:55:45 -0500

    Removed django-safedelete from Product model and replaced with deleted boolean field

--- stockupapi/models/product.py
+++ stockupapi/models/product.py
@@ -1,13 +1,12 @@
+from django.db import models
 from django.db.models.deletion import CASCADE
-from django.db.models.fields import CharField
+from django.db.models.fields import BooleanField, CharField
 from django.db.models.fields.files import ImageField
 from django.db.models.fields.related import ForeignKey, ManyToManyField
-from safedelete.models import SafeDeleteModel
-from safedelete.models import SOFT_DELETE
 
-class Product(SafeDeleteModel):
-    _safedelete_policy = SOFT_DELETE
+class Product(models.Model):
     name = CharField(max_length=150)
     company = ForeignKey("Company", on_delete=CASCADE)
     image = ImageField(upload_to='products', height_field=None, width_field=None, max_length=None, null=True)
     parts = ManyToManyField("CompanyPart", through="ProductPart", related_name="products")
+    deleted = BooleanField(default=False)
