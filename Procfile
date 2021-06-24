249471231af51c9523722a0cd2596f3aeaf900c9
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-23 19:02:13 -0500

    Changes for deployemnt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn stockup.wsgi --log-file -
--- stockup/settings.py
+++ stockup/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -133,3 +133,8 @@ APPEND_SLASH = False
 
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
+django_on_heroku.settings(locals())
