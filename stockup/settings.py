6e57d1474c3b9c54c56031a722ccd6c559cd6b7c
kjk1325e <kaitlinjkelley@gmail.comm>
2021-06-24 10:54:49 -0500

    Changed whitelist to heroku url

--- stockup/settings.py
+++ stockup/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://stockupapp.netlify.app/'
+    'https://stockupclient.herokuapp.com'
 )
 
 ROOT_URLCONF = 'stockup.urls'
