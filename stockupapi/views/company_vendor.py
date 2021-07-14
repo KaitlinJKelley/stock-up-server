562e3103e957769dbfc528cc41fe82d14765cd7e
kjk1325e <kaitlinjkelley@gmail.comm>
2021-07-14 16:46:19 -0500

    Added update function to CompanyVendorViewSet

--- stockupapi/views/company_vendor.py
+++ stockupapi/views/company_vendor.py
@@ -1,7 +1,8 @@
+from stockupapi.models import company_vendor
 from stockupapi.models.company_vendor import CompanyVendor
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework import serializers
+from rest_framework import serializers, status
 from stockupapi.models import Vendor, Company
 
 class CompanyVendorViewSet(ViewSet):
@@ -22,6 +23,21 @@ class CompanyVendorViewSet(ViewSet):
         serializer = CompanyVendorSerializer(company_vendor, context={'request': request})
 
         return Response(serializer.data)
+
+    def update(self, request, pk):
+        company = Company.objects.get(employee__user = request.auth.user)
+
+        company_vendor = CompanyVendor.objects.get(pk=pk, company=company)
+
+        company_vendor.sales_rep_name = request.data["salesRepName"]
+        company_vendor.sales_rep_phone = request.data["salesRepPhone"]
+        company_vendor.address = request.data["address"]
+        company_vendor.login_username = request.data["loginUsername"]
+        company_vendor.login_password = request.data["loginPassword"]
+
+        company_vendor.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
 
 class VendorSerializer(serializers.ModelSerializer):
     
