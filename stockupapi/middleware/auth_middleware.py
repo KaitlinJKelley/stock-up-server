6a3aaf415e9df2220f031d8b93da457959793d05
kjk1325e <kaitlinjkelley@gmail.comm>
2021-08-04 17:42:48 -0500

    Refactored to only recycle token of requesting user if needed

--- stockupapi/middleware/auth_middleware.py
+++ stockupapi/middleware/auth_middleware.py
@@ -1,6 +1,9 @@
+from stockupapi.views.auth import login_user
 from rest_framework.authtoken.models import Token
 from datetime import datetime, timedelta
 from django.contrib.auth.models import User
+import json
+import pytz
 
 class AuthTokenMiddleware(object):
     def __init__(self, get_response):
@@ -12,25 +15,55 @@ class AuthTokenMiddleware(object):
         Code to be executed for each request before the view (and later
         middleware) are called.
         """
-        now = datetime.utcnow()
 
-        token_life = timedelta(hours=24)
+        utc = pytz.UTC
 
-        tokens = Token.objects.filter(created__lte=now - token_life)
+        now = datetime.now()
 
-        request.reset = False
+        token_life = timedelta(seconds=24)
 
-        for token in tokens:
+        recycle_date = utc.localize(now - token_life)
 
-            Token.objects.get(user_id=token.user_id).delete()
+        try:
+            _, user_token = request.META["HTTP_AUTHORIZATION"].split(" ")
+                
+            token = Token.objects.get(key=user_token)
 
-            new_token = Token.objects.create(user_id=token.user_id)
+        except Token.DoesNotExist:
+            # If sone passes a token that doesn't exist after being logged in, the reset property will cause the user to be 
+            # redirected to login on the client side regardless of the token
+            token = Token.objects.get(user_id=7)
 
-            if request.META["HTTP_AUTHORIZATION"] == f"Token {token}":
-                request.reset = True
+            request.META["HTTP_AUTHORIZATION"]= f"Token {token.key}"
+
+            request.reset = True
+
+            response = self.get_response(request)
+
+            return response
+
+        except:
+            login = login_user(request)
+
+            token_dict = json.loads(login.content.decode("utf-8"))
+
+            token = Token.objects.get(key=token_dict["token"])
+
+        if token.created < recycle_date:
+
+                user_id = token.user_id
+
+                token.delete()
+
+                new_token = Token.objects.create(user_id=user_id)
 
                 request.META["HTTP_AUTHORIZATION"]= f"Token {new_token}"
 
+                request.reset = True
+
+                print("TOKEN RECYCLED")
+        else:
+            token.reset = False
             
         response = self.get_response(request)
         return response
